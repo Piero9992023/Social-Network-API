df2271fdef1857ae7c975602bbb2b5495acaf38a
Piero Rodriguez <piero@Pieros-MacBook-Pro.local>
2023-11-27 18:00:00 -0500

    added reactionSchema to the thought model and deleted reaction file in models to fix reactionSchema not being defined error

--- models/Thought.js
+++ models/Thought.js
@@ -1,5 +1,41 @@
 const { Schema, model } = require('mongoose');
 
+const reactionSchema = new Schema(
+
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId(),
+        },
+
+        reactionBody: {
+            type: String,
+            required: true,
+            maxlength: 280,
+        },
+
+        username: {
+            type: String,
+            required: true,
+        },
+
+        createdAt: {
+            type: Date,
+            default: Date.now
+
+        }
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+        
+        id: false,
+    },
+);
+
+
 const thoughtSchema = new Schema(
     {
         thoughtText: {
